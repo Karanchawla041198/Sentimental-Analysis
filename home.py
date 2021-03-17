a410766809d1a66797be82341dc36ec6502a38af
Karanchawla041198 <43615756+Karanchawla041198@users.noreply.github.com>
2021-03-17 20:53:28 +0530

    development phase

--- home.py
+++ home.py
@@ -1,6 +1,6 @@
 import tkinter
 from tkinter import *
-
+from PIL import ImageTk,Image
 
 
 def head():
@@ -38,6 +38,18 @@ def generatetwitterf():
     twitterf = Frame(window, bg="grey", height=580, width=820)
     twitterf.place(x=180, y=120)
     Label(twitterf, text="This is Twitter page").place(x=0, y=0)
+    Label(twitterf, text="Twitter Sentiment", font=("Arial", 30)).place(x=230, y=10)
+    #photo = PhotoImage(file=r"twitter-icon.png")
+    #Label(twitterf, image=photo).place(x=150, y=200)
+
+    Label(twitterf, text="Enter Keyword: ", font=("Arial", 18)).place(x=120, y=150)
+    keywordentry = tkinter.Entry (twitterf,font=("Arial", 18))
+    keywordentry.place(x=380, y=150)
+
+    Label(twitterf, text="Enter No of Tweets: ", font=("Arial", 18)).place(x=120, y=200)
+    tweetsentry = tkinter.Entry(twitterf, font=("Arial", 18))
+    tweetsentry.place(x=380, y=200)
+
 
 def generatetrendsf():
     trendf = Frame(window, bg="green", height=580, width=820)
