ebc2b62195bd0a27f1574b17ff2867ba60f18d95
Karanchawla041198 <43615756+Karanchawla041198@users.noreply.github.com>
2021-03-17 20:22:19 +0530

    Commit 1

---
+++ home.py
@@ -0,0 +1,57 @@
+import tkinter
+from tkinter import *
+
+
+
+def head():
+    headf = Frame(window, bg="red",height=120, width=1000)
+    headf.place(x=0,y=0)
+    heading = Label(headf, text="Sentimental Analysis",font=('Arial Bold',30))
+    heading.place(x=300,y=35)
+
+def nav():
+    navf = Frame(window, bg="yellow", height=580, width=180)
+    navf.place(x=0,y=120)
+
+    b1=Button(navf,text="Home",width=23,height=9,bg="#b6ddfc",activebackground="#8bcafc",command=generatehomef)
+    b1.place(x=2,y=0)
+    b2 = Button(navf, text="About",width=23,height=9,bg="#b6ddfc",activebackground="#8bcafc",command=generateaboutf)
+    b2.place(x=2,y=145)
+    b3 = Button(navf, text="Twitter Sentiment",width=23,height=9,bg="#b6ddfc",activebackground="#8bcafc",command=generatetwitterf)
+    b3.place(x=2,y=290)
+    b4 = Button(navf, text="Trend Sentiemnt",width=23,height=9,bg="#b6ddfc",activebackground="#8bcafc",command=generatetrendsf)
+    b4.place(x=2,y=435)
+
+
+def generatehomef():
+    homef = Frame(window, bg="orange", height=580, width=820)
+    homef.place(x=180, y=120)
+    Label(homef,text="This is home page").place(x=0,y=0)
+
+
+def generateaboutf():
+    aboutf = Frame(window, bg="pink", height=580, width=820)
+    aboutf.place(x=180, y=120)
+    Label(aboutf, text="This is About page").place(x=0, y=0)
+
+def generatetwitterf():
+    twitterf = Frame(window, bg="grey", height=580, width=820)
+    twitterf.place(x=180, y=120)
+    Label(twitterf, text="This is Twitter page").place(x=0, y=0)
+
+def generatetrendsf():
+    trendf = Frame(window, bg="green", height=580, width=820)
+    trendf.place(x=180, y=120)
+    Label(trendf, text="This is Trend page").place(x=0, y=0)
+
+
+window = tkinter.Tk()
+window.geometry("1000x700")
+head()
+nav()
+
+
+generatehomef()
+
+
+window.mainloop()
---
+++ main.py
@@ -0,0 +1,139 @@
+import re
+import tweepy
+from tweepy import OAuthHandler
+from textblob import TextBlob
+
+class TwitterClient(object):
+	'''
+	Generic Twitter Class for sentiment analysis.
+	'''
+	def _init_(self):
+		'''
+		Class constructor or initialization method.
+		'''
+		# keys and tokens from the Twitter Dev Console
+		consumer_key = 'xU4wrrmSyj2Kf3isOFqr13nvO'
+		consumer_secret = 'JMHtomNpxjP3675GsV9hH1YCa6tHuR5LFLHcTbLv4fWH8DHGJM'
+		access_token = '1193117792796413952-2TO4tQ5rQdQHrnYVQbTj6KkSncZE5P'
+		access_token_secret = '7o91Uk9kMszRu4KjG9yibE5mc1WXJSc8eI8vR7rvQJKpm'
+
+		# attempt authentication
+		try:
+			# create OAuthHandler object
+			self.auth = OAuthHandler(consumer_key, consumer_secret)
+			# set access token and secret
+			self.auth.set_access_token(access_token, access_token_secret)
+			# create tweepy API object to fetch tweets
+			self.api = tweepy.API(self.auth)
+		except:
+			print("Error: Authentication Failed")
+
+	def clean_tweet(self, tweet):
+		'''
+		Utility function to clean tweet text by removing links, special characters
+		using simple regex statements.
+		'''
+		return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) |(\w+:\/\/\S+)", " ", tweet).split())
+
+	def get_tweet_sentiment(self, tweet):
+		'''
+		Utility function to classify sentiment of passed tweet
+		using textblob's sentiment method
+		'''
+		# create TextBlob object of passed tweet text
+		analysis = TextBlob(self.clean_tweet(tweet))
+		# set sentiment
+		if analysis.sentiment.polarity > 0:
+			output = open("twitter-out.txt", "a")
+			output.write("pos")
+			output.write("\n")
+			output.close()
+			return 'positive'
+		elif analysis.sentiment.polarity == 0:
+			output = open("twitter-out.txt", "a")
+			output.write("ooo")
+			output.write("\n")
+			output.close()
+			#return 'negative'
+			return 'neutral'
+		else:
+			output = open("twitter-out.txt", "a")
+			output.write("neg")
+			output.write("\n")
+			output.close()
+			return 'negative'
+
+	def get_tweets(self, query, count = 10):
+		'''
+		Main function to fetch tweets and parse them.
+		'''
+		# empty list to store parsed tweets
+		tweets = []
+
+		try:
+			# call twitter api to fetch tweets
+			fetched_tweets = self.api.search(q = query, count = count)
+
+			# parsing tweets one by one
+			for tweet in fetched_tweets:
+				# empty dictionary to store required params of a tweet
+				parsed_tweet = {}
+
+				# saving text of tweet
+				parsed_tweet['text'] = tweet.text
+				# saving sentiment of tweet
+				parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text)
+
+				# appending parsed tweet to tweets list
+				if tweet.retweet_count > 0:
+					# if tweet has retweets, ensure that it is appended only once
+					if parsed_tweet not in tweets:
+						tweets.append(parsed_tweet)
+				else:
+					tweets.append(parsed_tweet)
+
+			# return parsed tweets
+			return tweets
+
+		except tweepy.TweepError as e:
+			# print error (if any)
+			print("Error : " + str(e))
+
+def main():
+	# creating object of TwitterClient Class
+	api = TwitterClient()
+	# calling function to get tweets
+	print("Enter the keword for sentimental analysis")
+	s=input()
+	print("Enter the number of tweets")
+	number=(int(input()))
+	tweets = api.get_tweets(query = s, count = number)
+	print(tweets)
+	# picking positive tweets from tweets
+	ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
+	# percentage of positive tweets
+	print("Positive tweets percentage: {} %".format(100*len(ptweets)/len(tweets)))
+	# picking negative tweets from tweets
+	ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
+	# percentage of negative tweets
+	print("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets)))
+	# percentage of neutral tweets
+	otweets = [tweet for tweet in tweets if tweet['sentiment'] == 'neutral']
+	# percentage of negative tweets
+	print("Nutral tweets percentage: {} %".format(100 * len(otweets) / len(tweets)))
+
+
+	# printing first 5 positive tweets
+	print("\n\nPositive tweets:")
+	for tweet in ptweets[:10]:
+		print(tweet['text'])
+
+	# printing first 5 negative tweets
+	print("\n\nNegative tweets:")
+	for tweet in ntweets[:10]:
+		print(tweet['text'])
+
+if __name__ == "__main__":
+	# calling main function
+	open('twitter-out.txt', 'w').close()
+	main()
