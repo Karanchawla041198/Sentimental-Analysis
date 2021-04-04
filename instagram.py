7189fd7601d38c23cb637588d5433015d59f9d6c
charchil gupta <charchil99gupta@gmail.com>
2021-04-04 11:29:54 +0530

    add instagram anlaysis
    add instagram anlaysis
    
    Co-Authored-By: Karanchawla041198 <43615756+Karanchawla041198@users.noreply.github.com>

---
+++ instagram.py
@@ -0,0 +1,25 @@
+target_profile ="charchil99"
+
+from instaloader import Instaloader, Profile
+loader = Instaloader()
+
+profile = Profile.from_username(loader.context, target_profile)
+
+num_followers = profile.followers
+num_following=profile.biography
+x=profile.full_name
+print(num_following)
+print(num_followers)
+total_num_likes = 0
+total_num_comments = 0
+total_num_posts = 0
+
+for post in profile.get_posts():
+    total_num_likes += post.likes
+    total_num_comments += post.comments
+    total_num_posts += 1
+    if(total_num_posts==10):
+        break;
+
+engagement = float(total_num_likes + total_num_comments) / (num_followers * total_num_posts)
+print(engagement * 100)
